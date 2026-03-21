40811c8773be5455e278c3d9932003480dae681d
Charles Vestal <charles@charles.pizza>
2026-03-21 13:21:23 +0100

    chore: update paths and references for Schwung rename

--- src/dsp/superarp.c
+++ src/dsp/superarp.c
@@ -15,7 +15,7 @@
 #define MAX_ARP_NOTES 16
 #define DEFAULT_BPM 120
 #define SUPERARP_DEBUG_LOG 0
-#define SUPERARP_LOG_PATH "/data/UserData/move-anything/superarp.log"
+#define SUPERARP_LOG_PATH "/data/UserData/schwung/superarp.log"
 #define CLOCK_START_GRACE_TICKS 1
 #define CLOCK_ARP_OUTPUT_DELAY_TICKS 1
 #define MAX_PATTERN_STEPS 16
